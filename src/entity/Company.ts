3b87cc613ef12c4fc6a5bff69e911a670b01b58e
Walkerontheway <1521481790@qq.com>
2020-02-25 15:52:22 +0800

    1

---
+++ src/entity/Company.ts
@@ -0,0 +1,47 @@
+import {DataPage} from "@/entity/Base";
+import {SysUser} from "@/entity/Sys";
+
+
+export class Company extends DataPage {
+
+    public id!: bigint;
+
+    public name!: string;
+
+    public description!: string;
+
+    public user: SysUser = new SysUser();
+
+    public users!: Set<SysUser>;
+
+}
+
+export class Department extends DataPage {
+
+    public id!: bigint;
+
+    public name!: string;
+
+    public description!: string;
+
+    public company!: Company;
+
+    public parent!: Department;
+
+    public users!: Set<SysUser>;
+
+}
+
+export class Position extends DataPage {
+
+    public id!: bigint;
+
+    public name!: string;
+
+    public description!: string;
+
+    public department!: Department;
+
+    public users!: Set<SysUser>;
+
+}
