5c1dab8173c29a298c5760e218f9a4109eff677b
Walkerontheway <1521481790@qq.com>
2020-02-26 15:29:33 +0800

    修改数据源

--- src/entity/DataSource.ts
+++ src/entity/DataSource.ts
@@ -1,4 +1,36 @@
 import {SysUser} from "@/entity/Sys";
+import {DataPage} from "@/entity/Base";
+
+export class DataSource extends DataPage {
+
+    public id!: bigint;
+
+    public user!: SysUser;
+
+    public name !: string;
+
+    public type !: bigint;
+
+    public parentId !: bigint;
+
+    public url!: string;
+
+    public serverAddress !: string;
+
+    public databaseName !: string;
+
+    public port !: bigint;
+
+    public username !: string;
+
+    public password !: string;
+
+    public driverClass !: string;
+
+    public sId!: string
+
+}
+
 
 export class DataSourceLog {
 
@@ -12,3 +44,4 @@ export class DataSourceLog {
 
     public user!: SysUser;
 }
+
